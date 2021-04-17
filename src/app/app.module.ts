4f14ac502273bbee9ea365ddd47a559f618147f1
Thulasiram Siripireddy <>
2021-04-17 11:42:53 -0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+﻿import { NgModule, APP_INITIALIZER } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers';
+
+import { AppRoutingModule } from './app-routing.module';
+import { JwtInterceptor, ErrorInterceptor, appInitializer } from './_helpers';
+import { AccountService } from './_services';
+import { AppComponent } from './app.component';
+import { AlertComponent } from './_components';
+import { HomeComponent } from './home';
+
+import {VgCoreModule} from '@videogular/ngx-videogular/core';
+import {VgControlsModule} from '@videogular/ngx-videogular/controls';
+import {VgOverlayPlayModule} from '@videogular/ngx-videogular/overlay-play';
+import {VgBufferingModule} from '@videogular/ngx-videogular/buffering';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        AppRoutingModule,
+        VgCoreModule,
+        VgControlsModule,
+        VgOverlayPlayModule,
+        VgBufferingModule
+    ],
+    declarations: [
+        AppComponent,
+        AlertComponent,
+        HomeComponent
+    ],
+    providers: [
+        { provide: APP_INITIALIZER, useFactory: appInitializer, multi: true, deps: [AccountService] },
+        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+        // provider used to create fake backend
+        fakeBackendProvider
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,60 @@
+﻿import { Component } from '@angular/core';
+
+import { AccountService } from '@app/_services';
+
+@Component({ templateUrl: 'home.component.html' })
+export class HomeComponent {
+    account = this.accountService.accountValue;
+    videoItems = [
+        {
+            name: 'Video one',
+            src: 'http://static.videogular.com/assets/videos/videogular.mp4',
+            type: 'video/mp4'
+        },
+        {
+            name: 'Video two',
+            src: 'http://static.videogular.com/assets/videos/big_buck_bunny_720p_h264.mov',
+            type: 'video/mp4'
+        },
+        {
+            name: 'Video three',
+            src: 'http://static.videogular.com/assets/videos/elephants-dream.mp4',
+            type: 'video/mp4'
+        }
+    ];
+
+    activeIndex = 0;
+    currentVideo = this.videoItems[this.activeIndex];
+    data;
+
+    constructor(private accountService: AccountService) { }
+
+    ngOnInit(): void { }
+
+    videoPlayerInit(data) {
+        this.data = data;
+
+        this.data.getDefaultMedia().subscriptions.loadedMetadata.subscribe(this.initVdo.bind(this));
+        this.data.getDefaultMedia().subscriptions.ended.subscribe(this.nextVideo.bind(this));
+    }
+
+    nextVideo() {
+        this.activeIndex++;
+
+        if (this.activeIndex === this.videoItems.length) {
+            this.activeIndex = 0;
+        }
+
+        this.currentVideo = this.videoItems[this.activeIndex];
+    }
+
+    initVdo() {
+        //this.data.play();
+    }
+
+    startPlaylistVdo(item, index: number) {
+        this.activeIndex = index;
+        this.currentVideo = item;
+    }
+   
+}
